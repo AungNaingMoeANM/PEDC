906dabe5a8fe585cd9529d2ecae12e942b7677f2
AungNaingMoeANM <aungnaingmoedeveloper@gmail.com>
2023-03-12 21:19:09 +0700

    admin sign in form design

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminLoginComponent } from './login/admin-login/admin-login.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminLoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
