7570a32119d0e2b4efb928ea764b732fc70c268a
AungNaingMoeANM <aungnaingmoedeveloper@gmail.com>
2023-10-02 21:10:00 +0700

    upgrade sign in design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './login/admin-login/admin-login.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLoginComponent
+    AdminLoginComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
